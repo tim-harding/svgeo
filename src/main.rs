c16b720ae2e065470cf38c48d376574890ef1ce4
Tim Harding <Tim@TimHarding.co>
2024-09-13 10:10:40 -0700

    Reading SVG

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, Read};
+use usvg::{Options, Tree};
+
+fn main() -> anyhow::Result<()> {
+    let mut input = vec![];
+    stdin().read_to_end(&mut input)?;
+    let svg = Tree::from_data(input.as_slice(), &Options::default());
+    Ok(())
 }
