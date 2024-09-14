b7779f961ee3970547db9068afff1f5559fc4c78
Tim Harding <Tim@TimHarding.co>
2024-09-14 13:27:42 -0700

    Attempt at iterator based flattening

---
+++ src/json.rs
@@ -0,0 +1,94 @@
+use std::collections::HashMap;
+
+struct Point(f32, f32, f32);
+
+#[derive(Clone, Debug, PartialEq, PartialOrd)]
+enum Value {
+    String(String),
+    Str(&'static str),
+    Integer(u64),
+    Float(f64),
+    Boolean(bool),
+    Array(ValueVec),
+    Object(ValueObj),
+}
+
+macro_rules! value_from {
+    ($t:ty, $i:ident) => {
+        impl From<$t> for Value {
+            fn from(value: $t) -> Self {
+                Value::$i(value)
+            }
+        }
+    };
+}
+
+value_from!(String, String);
+value_from!(&'static str, Str);
+value_from!(u64, Integer);
+value_from!(f64, Float);
+value_from!(bool, Boolean);
+value_from!(ValueVec, Array);
+value_from!(ValueObj, Object);
+
+#[derive(Clone, Debug, Default, PartialEq, PartialOrd)]
+struct ValueVec(Vec<Value>);
+
+impl ValueVec {
+    pub const fn new() -> Self {
+        Self(vec![])
+    }
+
+    pub fn push(&mut self, value: impl Into<Value>) {
+        self.0.push(value.into());
+    }
+}
+
+macro_rules! value_vec {
+    ($($e:expr),*) => {
+        {
+            let mut out = ValueVec::new();
+            $(out.push($e);)*
+            out
+        }
+    };
+}
+
+#[derive(Clone, Debug, Default, PartialEq)]
+struct ValueObj(Vec<(&'static str, Value)>);
+
+impl ValueObj {
+    pub const fn new() -> Self {
+        Self(vec![])
+    }
+
+    pub fn insert(&mut self, key: &'static str, value: Value) {
+        self.0.push((key, value));
+    }
+}
+
+macro_rules! value_obj {
+    ($($k:expr; $v:expr),*) => {
+        {
+            let mut out = ValueObj::new();
+            $(out.push($k, $v);)*
+            out
+        }
+    };
+}
+
+fn structure() -> Value {
+    value_vec![
+        "fileversion",
+        "20.5.332",
+        "hasindex",
+        false,
+        "pointcount",
+        52,
+        "vertexcount",
+        52,
+        "primitivecount",
+        2,
+    ]
+    .into()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,63 @@
 use std::io::{stdin, Read};
 use usvg::{tiny_skia_path::PathSegment, Group, Node, Options, Tree};
 
+mod json;
+
+struct Point(f32, f32, f32);
+
+struct Primitive {
+    id: String,
+    segments: Vec<PathSegment>,
+}
+
 fn main() -> anyhow::Result<()> {
     let mut input = vec![];
     stdin().read_to_end(&mut input)?;
     let svg = Tree::from_data(input.as_slice(), &Options::default())?;
-    let mut segments = vec![];
-    get_segments(svg.root(), &mut segments);
+    let primitives = iter_primitives(svg.root());
     Ok(())
+}
+
+fn iter_primitives<I>(group: &Group) -> I
+where
+    I: Iterator<Item = Primitive>,
+{
+    group
+        .children()
+        .into_iter()
+        .map(|child| -> FlatMapIter<Primitive, I> {
+            match child {
+                Node::Group(group) => FlatMapIter::Multi(iter_primitives(group.as_ref())),
+                Node::Path(path) => FlatMapIter::Single(path.is_visible().then(|| Primitive {
+                    id: path.id().into(),
+                    segments: path.data().segments().collect(),
+                })),
+                Node::Image(_) | Node::Text(_) => FlatMapIter::Single(None),
+            }
+        })
+        .flatten()
+}
+
+enum FlatMapIter<T, I>
+where
+    I: Iterator<Item = T>,
+{
+    Single(Option<T>),
+    Multi(I),
+}
+
+impl<T, I> Iterator for FlatMapIter<T, I>
+where
+    I: Iterator<Item = T>,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            FlatMapIter::Single(single) => single.take(),
+            FlatMapIter::Multi(multi) => multi.next(),
+        }
+    }
 }
 
 fn get_segments(group: &Group, segments: &mut Vec<PathSegment>) {
