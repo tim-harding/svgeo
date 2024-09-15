a16c32ebd83d16634b0682ca95e23dcae3ff50e6
Tim Harding <Tim@TimHarding.co>
2024-09-14 18:30:43 -0700

    Simplify paths to just point arrays

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
-    default,
     io::{stdin, Read},
+    ops::{Add, Div, Mul, Sub},
 };
 use usvg::{
     tiny_skia_path::{PathSegment, Point},
@@ -63,17 +63,16 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    // Only support quad + closed to begin with
     let mut prims = vec![];
     for path in paths {
-        let mut prim = None;
+        let mut prim: Option<PrimBuilder> = None;
         for segment in path.segments.into_iter() {
             match segment {
                 PathSegment::MoveTo(p0) => {
                     if let Some(prim) = prim {
-                        prims.push(prim);
+                        prims.push(prim.build());
                     }
-                    prim = Some(Prim::new(p0.into()));
+                    prim = Some(PrimBuilder::new(p0.into()));
                 }
                 PathSegment::LineTo(p0) => {
                     if let Some(prim) = &mut prim {
@@ -96,7 +95,7 @@ fn main() -> anyhow::Result<()> {
                 PathSegment::Close => {
                     if let Some(mut prim) = prim.take() {
                         prim.is_closed = true;
-                        prims.push(prim);
+                        prims.push(prim.build());
                     }
                 }
             }
@@ -125,6 +124,38 @@ impl From<Point> for P {
     }
 }
 
+impl Sub for P {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl Add for P {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl Mul<f32> for P {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self(self.0 * rhs, self.1 * rhs)
+    }
+}
+
+impl Div<f32> for P {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Self(self.0 / rhs, self.1 / rhs)
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 enum Segment {
     Line(P),
@@ -132,21 +163,84 @@ enum Segment {
     Cube(P, P, P),
 }
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Default)]
-struct Prim {
-    start: P,
-    order: Order,
-    segments: Vec<Segment>,
-    is_closed: bool,
+const R13: f32 = 1.0 / 3.0;
+const R23: f32 = 2.0 / 3.0;
+
+impl Segment {
+    pub fn to_cube(self, p0: P) -> [P; 3] {
+        match self {
+            Segment::Line(p1) => {
+                let d = p1 - p0;
+                [p0 + d * R13, p0 + d * R23, p1]
+            }
+            Segment::Quad(p1, p2) => [p0 + (p1 - p0) * R23, p2 + (p1 - p2) * R23, p2],
+            Segment::Cube(p1, p2, p3) => [p1, p2, p3],
+        }
+    }
+
+    pub fn to_quad(self, p0: P) -> [P; 2] {
+        match self {
+            Segment::Line(p1) => [(p0 + p1) / 2.0, p1],
+            Segment::Quad(p1, p2) => [p1, p2],
+            Segment::Cube(_, _, _) => panic!("Can't convert cube to quad"),
+        }
+    }
 }
 
-impl Prim {
+#[derive(Debug, Clone, PartialEq, PartialOrd, Default)]
+struct PrimBuilder {
+    order: Order,
+    is_closed: bool,
+    start: P,
+    segments: Vec<Segment>,
+}
+
+impl PrimBuilder {
     pub fn new(start: P) -> Self {
         Self {
             start,
             ..Default::default()
         }
     }
+
+    pub fn build(self) -> Prim {
+        let mut points = vec![self.start];
+        match self.order {
+            Order::Line => {
+                for segment in self.segments {
+                    let Segment::Line(p0) = segment else {
+                        panic!("Expected a line");
+                    };
+                    points.push(p0);
+                }
+            }
+            Order::Quad => {
+                for segment in self.segments {
+                    points.extend(segment.to_quad(*points.last().unwrap()));
+                }
+            }
+            Order::Cube => {
+                for segment in self.segments {
+                    points.extend(segment.to_cube(*points.last().unwrap()));
+                }
+            }
+        }
+        if self.is_closed {
+            points.pop();
+        }
+        Prim {
+            order: self.order,
+            is_closed: self.is_closed,
+            points,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default, PartialEq, PartialOrd)]
+struct Prim {
+    order: Order,
+    is_closed: bool,
+    points: Vec<P>,
 }
 
 // [
